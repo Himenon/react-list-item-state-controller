74f5150f3163e698701e73504d4fb4f729ba1894
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-01 10:50:10 +0900

    chore: add source code (#1)

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import Item, { ItemProps } from "./Item";
+import "./App.css";
+
+const longTask = async (): Promise<void> => {
+  const randomTimeSeconds = Math.random() * 5000;
+  return new Promise<void>((resolve, reject) => {
+    window.setTimeout(() => {
+      const value = Math.floor(Math.random() * 100);
+      const isEvent = value % 2 === 0;
+      if (isEvent) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, randomTimeSeconds);
+  });
+};
+
+export type AppProps = {};
+
+const App: React.FC<AppProps> = () => {
+  console.log("Re-rendering: App");
+  const items: ItemProps[] = ["foo", "barr", "bob", "john"].map(
+    (name): ItemProps => {
+      return {
+        title: name,
+        onSubmit: (controller) => {
+          controller.setLoadingStatus("LOADING");
+          longTask()
+            .then(() => {
+              controller.setLoadingStatus("READY");
+              controller.setResult("SUCCESS");
+            })
+            .catch(() => {
+              controller.setLoadingStatus("READY");
+              controller.setResult("FAILED");
+            })
+            .finally(() => {
+              controller.setLoadingStatus("READY");
+            });
+        },
+      };
+    },
+  );
+
+  return (
+    <div>
+      <h2>List Item Controller sample</h2>
+      <ul className="list">
+        {items.map((item, index) => {
+          return <Item key={index.toString()} {...item} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Item.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+
+type Loading = "LOADING" | "READY";
+
+type Result = "SUCCESS" | "FAILED" | "UNKNOWN";
+
+type ItemController = {
+  setLoadingStatus: (status: Loading) => void;
+  setResult: (result: Result) => void;
+};
+
+export type ItemProps = {
+  title: string;
+  onSubmit: (controller: ItemController) => void;
+};
+
+const Item: React.FC<ItemProps> = (props) => {
+  console.log(`Re-rendering: ${props.title}`);
+  const [loadingStatus, setLoadingStatue] = React.useState<Loading>("READY");
+  const [result, setResult] = React.useState<Result>("UNKNOWN");
+
+  const itemController: ItemController = {
+    setLoadingStatus: (newLoadingStatus) => {
+      setLoadingStatue(newLoadingStatus);
+    },
+    setResult: (newResult) => {
+      setResult(newResult);
+    },
+  };
+  const handleClick = () => {
+    props.onSubmit(itemController);
+  };
+  return (
+    <li>
+      <span className="title">{props.title}</span>
+      <span className="loading">Loading: {loadingStatus}</span>
+      <span className="result">Result: {result}</span>
+      <button onClick={handleClick} disabled={loadingStatus === "LOADING"}>
+        Submit
+      </button>
+    </li>
+  );
+};
+
+export default Item;
